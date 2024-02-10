56f98720ca8a13e0a7d01398e3cb0c850243c48f
Fatih <fatiharahmat257@gmail.com>
2024-02-11 00:14:11 +0700

    test show data

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,15 +1,18 @@
-// ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
-
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:xsis_movie/presentation/providers/movie/upcoming_provider.dart';
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+import '../misc/async_value_widget.dart';
+import '../providers/movie/movie_videos_provider.dart';
+
+class MainPage extends ConsumerStatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends ConsumerState<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +22,36 @@ class _MainPageState extends State<MainPage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: const Column(
-            children: [],
+          padding: const EdgeInsets.all(20.0),
+          child: AsyncValueWidget(
+            value: ref.watch(upcomingProvider),
+            data: (data) => Column(
+              children: [
+                ...data
+                    .map(
+                      (movie) => Text(movie.title),
+                    )
+                    .toList(),
+                const Text("text"),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                  ),
+                  onPressed: () {
+                    ref
+                        .read(movieVideosProvider.notifier)
+                        .getVideos(id: 866398);
+                  },
+                  child: const Text("detail"),
+                ),
+                AsyncValueWidget(
+                  value: ref.watch(movieVideosProvider),
+                  data: (data) => Text(
+                    data.map((e) => e.name).toList().toString(),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
