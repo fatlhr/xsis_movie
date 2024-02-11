24f41a0896dda450ef8dfc191e96354ec8f140c1
Fatih <fatiharahmat257@gmail.com>
2024-02-11 21:42:15 +0700

    add search widget

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -27,6 +27,49 @@ class _MainPageState extends ConsumerState<MainPage> {
         width: size.width,
         child: CustomScrollView(
           slivers: [
+            SliverPadding(
+              padding: const EdgeInsets.all(16),
+              sliver: SliverToBoxAdapter(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 6.0,
+                    horizontal: 12.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(12.0),
+                    ),
+                    border: Border.all(
+                      width: 1.0,
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(Icons.search),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          initialValue: null,
+                          decoration: const InputDecoration.collapsed(
+                            filled: true,
+                            fillColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            hintText: "Search",
+                          ),
+                          onFieldSubmitted: (value) {
+                            
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             ImageHorizontalSliverList(
               height: size.height / 3,
               title: 'Popular',
